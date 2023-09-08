5e7dffc74c15151148ceb78a4401ee24561a9a3a
Seyi Amusan <Seyijoshua14@gmail.com>
2023-09-08 18:45:13 +0100

    Completed project and pushed to GitHub

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const header = document.querySelector('.header')
 const inputEL = header.querySelector('.header__input');
 const btnEl = header.querySelector('.header__btn');
+const errorMsg = document.querySelector('.header__error-msg');
 const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+const displayError = () => {
+    errorMsg.style.display = 'block';
+    inputEL.style.borderColor = 'red';
+}
+
+const removeError = () => {
+    errorMsg.style.display = 'none';
+    inputEL.style.borderColor = 'black';
+}
+
 const validateEmail = () => {
-    if (btnEl.value.match(emailRegex)) return
-
-    let p = document.createElement('p');
-    p.textContent = 'Please provide a valid email'
-    header.appendChild(p);
-
+    if (!inputEL.value.match(emailRegex)) {
+        displayError();
+        return
+    }
+    removeError();
 }
 
 const initApp = () => {
